35265ee6c72a8533216ac7263a71701f10a17f9d
Władysław <89138444+vlzher@users.noreply.github.com>
2023-10-11 14:54:38 +0200

    first commit

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -13,7 +13,7 @@ const Modal = () => {
         <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
           <div className="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
             <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-              Static modal
+              Adding new Poll
             </h3>
             <button
               type="button"
@@ -40,13 +40,21 @@ const Modal = () => {
           </div>
           <div className="p-6 space-y-6">
             <div>
+              <div className="mb-5">
+                <label htmlFor="error" className="block mb-2 text-sm font-medium text-red-700 dark:text-red-500">Enter Poll name</label>
+                <input type="text" id="error"
+                       className="bg-red-50 border border-red-500 text-red-900 placeholder-red-700 text-sm rounded-lg focus:ring-red-500 dark:bg-gray-700 focus:border-red-500 block w-full p-2.5 dark:text-red-500 dark:placeholder-red-500 dark:border-red-500"
+/>                  <p className="mt-2 text-sm text-red-600 dark:text-red-500"><span
+                      className="font-medium">Oh, snapp!</span> Some error message.</p>
+              </div>
               <label
                 htmlFor="last_name"
                 className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
               >
                 Add poll field
               </label>
-              <div className="flex flex-row justify-between">
+
+              <div className="flex flex-row justify-between mb-5">
                 <input
                   type="text"
                   id="last_name"
@@ -60,6 +68,17 @@ const Modal = () => {
                   Add
                 </button>
               </div>
+                <div>
+                    <ul className="w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+                        <div className="w-full flex-row flex justify-between items-center px-4 py-2 border-b border-gray-200 rounded-t-lg dark:border-gray-600"><li>Profile</li><button
+                            className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
+                        >
+                            Remove
+                        </button></div>
+
+                    </ul>
+
+                </div>
             </div>
           </div>
           <div className="flex justify-end items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
